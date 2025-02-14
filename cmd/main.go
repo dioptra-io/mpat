f5a894dce3e97ad09789723c19d25dcafa20daa3
Ufuk Bombar <ufukbombar@gmail.com>
2025-02-14 23:04:16 +0100

    Add logger

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
 	"dioptra-io/ufuk-research/internal/iris"
+	"dioptra-io/ufuk-research/internal/log"
 )
 
 var (
@@ -15,12 +17,31 @@ var (
 	fUser           string
 	fPassword       string
 	fClickhouseHost string
+	fSilent         bool
+	fDebug          bool
 )
+
+var logger = log.GetLogger()
 
 var rootCmd = &cobra.Command{
 	Use:   "mpat [module name]",
-	Short: "Run the analysisctl",
+	Short: "MPAT: Measurement Platform Analysis Tool",
 	Long:  "",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// Set the default arguments for logging
+		logger.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+		if fSilent {
+			logger.SetLevel(logrus.PanicLevel)
+		} else {
+			if fDebug {
+				logger.SetLevel(logrus.DebugLevel)
+			} else {
+				logger.SetLevel(logrus.InfoLevel)
+			}
+		}
+
+		logger.Debugln("RootCmd prerun succesfull.")
+	},
 }
 
 func init() {
@@ -34,17 +55,23 @@ func init() {
 		StringVarP(&fPassword, "password", "p", "", "set the name of the password")
 	rootCmd.PersistentFlags().
 		StringVarP(&fClickhouseHost, "host", "a", "localhost:9000", "set the host address and the port of the clickhouse")
+	rootCmd.PersistentFlags().
+		BoolVarP(&fSilent, "silent", "s", false, "set this flag to suppress logs")
+	rootCmd.PersistentFlags().
+		BoolVar(&fDebug, "debug", false, "set this flag to see debug messages, if silent is set then this flag is ignored")
 
 	// Bind the variables to viper
 	viper.BindPFlag("database", rootCmd.PersistentFlags().Lookup("database"))
 	viper.BindPFlag("user", rootCmd.PersistentFlags().Lookup("user"))
 	viper.BindPFlag("password", rootCmd.PersistentFlags().Lookup("password"))
 	viper.BindPFlag("host", rootCmd.PersistentFlags().Lookup("host"))
+	viper.BindPFlag("slent", rootCmd.PersistentFlags().Lookup("silent"))
+	viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug"))
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		logger.Panicf("fatal error encountered while running the command: %s", err)
 		os.Exit(1)
 	}
 }
---
+++ internal/log/log.go
@@ -0,0 +1,14 @@
+package log
+
+import "github.com/sirupsen/logrus"
+
+var log = logrus.New()
+
+func Init() {
+	log.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+	log.SetLevel(logrus.InfoLevel)
+}
+
+func GetLogger() *logrus.Logger {
+	return log
+}
